4247ccc3ad52e3e6b13324c29f809af7e3703fc0
tristanH77 <tristanhartman77@gmail.com>
2023-12-19 21:03:53 -0500

    add profile model

---
+++ models/profile.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the user who received the income
+    image: { type: String }, // Image of the user, if we need it or an Avatar
+    firstName: { type: String, required: true }, // First name of the user, if needed, will have to delete from "User" model
+    lastName: { type: String, required: true }, // Last name of the user, if needed, will have to delete from "User" model
+    age: { type: Number, required: true }, // Age of the user
+
+    calorieGoal: { type: Number }, // users daily calorie goal
+    macroGoals: {
+      proteinGoal: { type: Number }, // users daily protein goal
+      fatGoal: { type: Number }, //users daily fat goal
+      carbGoal: { type: Number },
+    }, //users daily fat goal
+
+    weightGoal: { type: Number }, //users weight goal
+    currentWeight: { type: Number }, //users current weight
+    currentCalories: { type: Number }, //users current calories for the day
+    currentMacros: {
+      currentProtein: { type: Number }, //users current protein for the day
+      currentFat: { type: Number }, //users current fat for the day
+      currentCarb: { type: Number }, //users current carbs for the day
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Profile", profileSchema);
