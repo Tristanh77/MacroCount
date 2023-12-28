b3bf72d5ef72b0e69404de6b5658d13c2cfe0be0
tristanH77 <tristanhartman77@gmail.com>
2023-12-27 21:11:03 -0500

    add meal model

---
+++ models/exercise.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user who created the expense
+  category: {
+    type: String,
+    required: true,
+  }, 
+  caloriesburned: { type: Number, required: true }, 
+}, {
+  timestamps: true
+});
+
+
+module.exports = mongoose.model('Exercise', exerciseSchema);
