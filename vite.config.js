103b4c0efe19e6860e03b997c3f403438476a0ff
Tristanh77 <tristanhartman77@gmail.com>
2024-06-02 18:25:55 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: "./src/main.jsx",
+      },
     },
-  },
+    server: {
+      port: 5173,
+      hmr: mode !== 'production',
+    },
+    envPrefix: 'VITE_', // Ensure the environment variable prefix is correct
+  };
 });
