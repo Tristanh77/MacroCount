1fae014e19a836c0c09f348311ea286dea099007
Tristanh77 <tristanhartman77@gmail.com>
2024-06-09 22:37:48 -0400

    prepped for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
-const cors = require("cors");
+// const cors = require("cors");
 
 require("./config/database");
 require("./src/utils/cronjobs");
@@ -13,11 +13,11 @@ app.use(logger("dev"));
 app.use(express.json());
 
 // Configure CORS
-const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:8000", "https://macro-count.vercel.app"], // Allow both local development and deployed domain
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: ["http://localhost:3000", "http://localhost:8000", "https://macro-count.vercel.app"], // Allow both local development and deployed domain
+//   optionsSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
 // Configure the auth middleware
 app.use(require("./config/auth"));
@@ -28,15 +28,19 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/goals", require("./routes/api/goals"));
 app.use("/api/exercise", require("./routes/api/exercise"));
 app.use("/api/meals", require("./routes/api/meals"));
+const manifest = require('./dist/manifest.json');
+app.use(express.static(path.join(__dirname, "dist")));
 
 // "catch all" route
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/*', function(req, res) {
+  res.render(path.join(__dirname, 'dist', 'index.ejs'), {manifest});
 });
 
-const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-  console.log(`> Local: http://localhost:${port}/`);
+const { PORT = 8000 } = process.env;
+app.listen(PORT, () => {
+  console.log();
+  console.log(`  App running in port ${PORT}`);
+  console.log();
+  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
 });
--- src/utils/userService.js
+++ src/utils/userService.js
@@ -1,22 +1,29 @@
-import tokenService from "./tokenService";
+import tokenService from './tokenService';
+// import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8000/api/";
-console.log("BASE_URL:", BASE_URL);
+const BASE_URL = '/api/users/';
+
 
 function signup(user) {
-  return fetch(BASE_URL + "users/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(user)
-  })
-    .then((res) => {
-      if (res.ok) return res.json();
-      throw new Error("Email already taken!");
+  return (
+    fetch(BASE_URL + "signup", {
+      method: "POST",
+      // headers: new Headers({'Content-Type': 'application/json'}),  // If you are sending a file/photo over
+      // what do datatype do you need to change this too?
+      body: user, // <- have to make sure when sending a file/photo, that the body is formData
     })
-    .then(({ token }) => tokenService.setToken(token));
-}
+      .then((res) => {
+        if (res.ok) return res.json();
+        // Probably a duplicate email
+        throw new Error("Email already taken!");
+      })
+      // Parameter destructuring!
+      // setting the token in localstorage!
+      // This is when we recieve the token from the server on the client
+      // and store it in localstorage
+      .then(({ token }) => tokenService.setToken(token))
+  );
+    }
 
 function getUser() {
   return tokenService.getUserFromToken();
@@ -27,17 +34,18 @@ function logout() {
 }
 
 function login(creds) {
-  return fetch(BASE_URL + "users/login", {
-    method: "POST",
-    headers: new Headers({ "Content-Type": "application/json" }),
-    body: JSON.stringify(creds),
+  return fetch(BASE_URL + 'login', {
+    method: 'POST',
+    headers: new Headers({'Content-Type': 'application/json'}),
+    body: JSON.stringify(creds)
   })
-    .then((res) => {
-      if (res.ok) return res.json();
-      throw new Error("Bad Credentials!");
-    })
-    .then(({ token }) => tokenService.setToken(token));
+  .then(res => {
+    if (res.ok) return res.json();
+    throw new Error('Bad Credentials!');
+  })
+  .then(({token}) => tokenService.setToken(token));
 }
+
 
 export default {
   signup,
