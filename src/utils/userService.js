2f3d304ba0b9c996a56dbb3f651394743c1aabfe
Tristanh77 <tristanhartman77@gmail.com>
2024-06-20 23:15:47 -0400

    fix signup and cutoff problem

--- src/utils/userService.js
+++ src/utils/userService.js
@@ -1,29 +1,22 @@
-import tokenService from './tokenService';
-// import axios from 'axios';
+import tokenService from "./tokenService";
 
 const BASE_URL = '/api/users/';
-
+console.log("BASE_URL:", BASE_URL);
 
 function signup(user) {
-  return (
-    fetch(BASE_URL + "signup", {
-      method: "POST",
-      // headers: new Headers({'Content-Type': 'application/json'}),  // If you are sending a file/photo over
-      // what do datatype do you need to change this too?
-      body: user, // <- have to make sure when sending a file/photo, that the body is formData
+  return fetch(BASE_URL + "signup", {  // Changed "users/signup" to "signup"
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      throw new Error("Email already taken!");
     })
-      .then((res) => {
-        if (res.ok) return res.json();
-        // Probably a duplicate email
-        throw new Error("Email already taken!");
-      })
-      // Parameter destructuring!
-      // setting the token in localstorage!
-      // This is when we recieve the token from the server on the client
-      // and store it in localstorage
-      .then(({ token }) => tokenService.setToken(token))
-  );
-    }
+    .then(({ token }) => tokenService.setToken(token));
+}
 
 function getUser() {
   return tokenService.getUserFromToken();
@@ -34,18 +27,17 @@ function logout() {
 }
 
 function login(creds) {
-  return fetch(BASE_URL + 'login', {
-    method: 'POST',
-    headers: new Headers({'Content-Type': 'application/json'}),
-    body: JSON.stringify(creds)
+  return fetch(BASE_URL + "login", {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    body: JSON.stringify(creds),
   })
-  .then(res => {
-    if (res.ok) return res.json();
-    throw new Error('Bad Credentials!');
-  })
-  .then(({token}) => tokenService.setToken(token));
+    .then((res) => {
+      if (res.ok) return res.json();
+      throw new Error("Bad Credentials!");
+    })
+    .then(({ token }) => tokenService.setToken(token));
 }
-
 
 export default {
   signup,
